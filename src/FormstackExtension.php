a6af9143b63deb8e849bcca797d7bef900f0079e
Michael Born <mborn319@gmail.com>
2018-08-21 17:39:53 -0400

    Add basic / dummy / pseudo-code for formstack API.
    This *should* work, but who knows because it's pretty hard to find the
    API setup in my Formstack dashboard.

--- src/FormstackExtension.php
+++ src/FormstackExtension.php
@@ -3,6 +3,7 @@
 namespace Bolt\Extension\Mborn319\Formstack;
 
 use Bolt\Extension\SimpleExtension;
+use \JGulledge\FormStack\API\FormStack;
 
 /**
  * ExtensionName extension class.
@@ -11,4 +12,45 @@ use Bolt\Extension\SimpleExtension;
  */
 class FormstackExtension extends SimpleExtension
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigFunctions() {
+        return [
+            'formstackEmbedForm' => 'formstackEmbedForm'
+        ];
+    }
+
+    /**
+     * Embed a Formstack form onto the page.
+     * 
+     * @return array
+     */
+    public function formstackEmbedForm( $form_id ) {
+        $config = $this->getConfig();
+
+
+        // begin Formstack API connection
+        $apiConfig = array(
+            'client_id'     => $config['api_client_id'],
+            'client_secret' => $config['api_client_secret'],
+            'redirect_url'  => $config['api_redirect_url'],
+            'access_token'  => $config['api_access_token']
+        );
+        $formStack = new FormStack($apiConfig);
+
+        // Duh - if debug is on, do debugging!
+        $app = $this->getAll();
+        if ( $app['debug'] ) {
+            $formStack->setDebug();
+        }
+
+        // get the form
+        $myForm = $formStack->loadForm( $form_id );
+        $details = $myForm->getDetails();
+
+        print( $details['html'] );
+
+        
+    }
 }
